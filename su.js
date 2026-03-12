920080d3f08bf13255263f409333119b5b2eab36
Harsh Sharma <harshsharma5183@gmail.com>
2026-03-12 16:50:56 +0530

    Add event listeners for GitHub and resume buttons

--- su.js
+++ su.js
@@ -1,1 +1,31 @@
 
+const githubBtn = document.getElementById("githubBtn")
+const resumeBtn = document.getElementById("resumeBtn")
+
+githubBtn.addEventListener("click", function () {
+
+window.open("https://github.com/yourusername", "_blank")
+
+})
+
+
+resumeBtn.addEventListener("click", function () {
+
+alert("Resume will be added soon")
+
+})
+
+
+const projectButtons = document.querySelectorAll(".projectBtn")
+
+projectButtons.forEach(function(button){
+
+button.addEventListener("click", function(){
+
+const link = button.getAttribute("data-link")
+
+window.open(link, "_blank")
+
+})
+
+})
